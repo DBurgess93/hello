686071f139664371a3bedbc253d94caf2cf946e9
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-08 15:07:07 +0930

    manual counter

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import { useState } from 'react'
 
 const App = () => {
   const [counter, setCounter] = useState(0)
-  setTimeout(
-    () => setCounter(counter + 1),
-    1000
-  )
 
-  console.log('rendering...', counter)
-  
   return (
     <div>
-      {counter}
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>
+        plus
+      </button>
+      <button onClick={() => setCounter(0)}>
+        zero
+      </button>
     </div>
   );
 }
