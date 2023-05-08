3a2ba8fbbdb6f7715ef11b80260ef4b8f9556425
Daniel Burgess <dan-burgess@hotmail.com>
2023-05-08 14:57:32 +0930

    Counter with useState hook

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import { useState } from 'react'
 
-const App = (props) => {
-  const {counter} = props
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
+
+  console.log('rendering...', counter)
+  
   return (
     <div>
       {counter}
--- src/index.js
+++ src/index.js
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-let counter = 1
-const refresh = () => {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <App counter={counter} />
-  )
-}
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
